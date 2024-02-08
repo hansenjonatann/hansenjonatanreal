6c5178a7bb04da8637872ebc37308db91d22e08d
Hansen Jonatan <114968570+hansenjonatann@users.noreply.github.com>
2024-02-09 01:27:20 +0700

    Update About.jsx
    Change the language to english

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,18 +12,18 @@ const About = () => {
           <div className="flex flex-wrap">
             <div className="w-full px-4 mb-10 lg:w-1/2">
               <h4 className="font-bold text-primary uppercase text-lg mb-3">
-                Tentang Saya
+                About me
               </h4>
               <h2 className="font-bold text-dark text-3xl mb-5 max-w-md lg:text-4xl">
-                Yuk, belajar web programming di HJ Codin
+                I am a student at Batam International University major in Information System
               </h2>
               <p className="font-medium text-base text-secondary max-w-xl lg:text-lg">
-                HJ Codin adalah sebuah bisnis yang saya dirikan untuk membantu pengguna dalam belajar pemograman web
+                I have a Community with name is HJ Codin , and i am also developing the HJ Codin's web and mobile app. if you want to see my community app , you can follow me on Instagram with the username is hansenjonatann . In Instagram I give the information when I am release the HJ Codin's Web and Mobile Application.
               </p>
             </div>
             <div className="w-full px-4 lg:w-1/2">
               <h3 className="font-semibold text-2xl text-dark mb-4 lg:text-3xl lg:pt-10">
-                Mari Berteman
+                Get in touch
               </h3>
               <p className="font-medium text-base text-secondary mb-6 lg:text-lg">
                 Saya adalah tipe orang yang terbuka , jika ada saran atau masukkan untuk saya dan website ini silahkan hubungi saya di sosial media di bawah ini!
