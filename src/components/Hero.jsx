88fe7ffca526855e512eb11f2ef4821ec313d6ee
Hansen Jonatan <114968570+hansenjonatann@users.noreply.github.com>
2024-02-09 01:42:44 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
               Student & <span className="text-dark">Content Creator</span>
             </h2>
             <p className="font-medium text-secondary mb-10 leading-relaxed">
-              A Student who has passion in technology and he has dream to become 
+              A student who has a passion for technology and dreams of becoming a {" "}
               <span className="text-dark font-semibold">
                  Fullstack  Developer and{" "}
                 <span className="text-primary font-semibold">
